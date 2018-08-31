ba56d7a6952a79732b8c08cc724bcb6288632cc5
jonathanbain1992 <2092528b@student.gla.ac.uk>
2018-08-31 23:46:10 +0100

    added functionality to get / remove node by it's hashcode, and also a tuple data struct which oddly was unable to find natively in Java.

--- src/Node.java
+++ src/Node.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 
 public class Node {
@@ -6,7 +7,8 @@ public class Node {
     private int authScore;
     private int weight;
 
-    private ArrayList<Node> adjacentNodes = new ArrayList<>();
+    //unbelievable that I had to make my own Tuple data structure. @TODO: see if any alternatives exist, despite stack overflow saying otherwise.
+    private ArrayList<Tuple> adjacentNodes = new ArrayList<>();
 
     private double getHashCode(){
         return this.hashCode();
@@ -58,44 +60,93 @@ public class Node {
     }
 
     /**
-     * Returns a Node specified if it exists in this Node's neighbourhood.
-     * @param  n  node to be returned
+     * Returns a Node specified if it exists in this Node's neighbourhood, can also get it's hashCode.
+     * @param  t  node to be returned
      * @return either a) the node itself, if it exists, b) an exception if the node was not in the node's neighbourhood
      * @see Node
      */
 
-    public Node getNeighbour(Node n) throws Exception {
-        if (this.adjacentNodes.contains(n)) {
-            return adjacentNodes.get(adjacentNodes.indexOf(n));
+    public Node getNeighbour(Tuple t) throws Exception {
+        if (this.adjacentNodes.contains(t)) {
+            return adjacentNodes.get(adjacentNodes.indexOf(t)).getLast();
         }
         else{
-            throw new Exception("Node: "+getHashCode()+"does not contain node: "+n.getHashCode()+"in it's adjacent neighbourhood.");
+            throw new Exception("Node: "+getHashCode()+"does not contain node: "+t.getLast().getHashCode()+"in it's adjacent neighbourhood.");
         }
     }
 
     /**
+     * Returns a Node specified by it's hashCode, if it exists in this Node's neighbourhood.
+     * @param  hashCode  node to be returned
+     * @return either a) the node itself, if it exists, b) an exception if the node was not in the node's neighbourhood
+     * @see Node
+     */
+
+    private Object getNeighbour(Double hashCode) throws Exception {
+        for(Tuple t : adjacentNodes) {
+            if (t.getFirst().equals(hashCode)) {
+                return adjacentNodes.get(adjacentNodes.indexOf(t)).getLast();
+            }
+            else {
+                throw new Exception("Node: " + getHashCode() + "does not contain node: " + hashCode + "in it's adjacent neighbourhood.");
+            }
+        }
+        return null;
+    }
+
+
+    /**
      * Returns the removed Node object if it exists in this Node's neighbourhood.
-     * @param  n  node to be removed
+     * @param  t tuple containing hashcode of node, and actual pointer to node to be removed
      * @return either a) the removed node, if it exists, b) an exception if the node was not in the node's neighbourhood and c) another
      * type of exception if any other error occurred, returning it's cause.
      * @see Node
      */
 
 
-    public Object removeNeighbour(Node n) throws Exception {
+    public Object removeNeighbour(Tuple t) throws Exception {
         try {
-            if (this.adjacentNodes.contains(n)) {
-                adjacentNodes.remove(n);
-                return n;
+            if (this.adjacentNodes.contains(t)) {
+                adjacentNodes.remove(t);
+                return t;
             }
             else {
-                throw new Exception("Node: " + getHashCode() + "does not contain node: " + n.getHashCode() + "in it's adjacent neighbourhood.");
+                throw new Exception("Node: " + getHashCode() + "does not contain node: " + t.getFirst() + " in it's adjacent neighbourhood.");
             }
         }
 
         catch (Exception e){
             throw new Exception(e);
         }
+    }
+
+    /**
+     * Returns hashCode of removed Node object if it exists in this Node's neighbourhood.
+     * @param  hashCode hasCode of node to be removed
+     * @return either a) the hashCode of the removed Node, if it exists, b) an exception if the node was not in the node's neighbourhood and c) another
+     * type of exception if any other error occurred, returning it's cause.
+     * @see Node
+     */
+
+    public Object removeNeighbour(Double hashCode) throws Exception {
+        try {
+            for(Tuple t: adjacentNodes) {
+                if (t.getFirst().equals(hashCode)) {
+                    adjacentNodes.remove(getNeighbour(hashCode));
+                    return hashCode;
+                }
+                else {
+                    throw new Exception("Node: " + getHashCode() + "does not contain node: " + hashCode + "in it's adjacent neighbourhood.");
+                }
+            }
+
+        }
+
+        catch (Exception e){
+            throw new Exception(e);
+
+        }
+        return null;
     }
 
     public Node(){
