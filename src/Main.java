239d50cc561c7172643230d51afe5cd6241fb82d
jonathanbain1992 <2092528b@student.gla.ac.uk>
2018-09-25 23:41:34 +0100

    partially integrated Edward Raff's LZJD implementation for incorporation to project.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        com.edwardraff.jlzjd.Main.main(args);
+        
+
     }
 
 }
